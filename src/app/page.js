0c66ba45763558e1d54bef9ccee7750c889c4e89
yosteric <yos.teban.2005@gmail.com>
2023-10-01 14:56:04 -0500

    Se arreglo bug visual en moviles

--- src/app/page.js
+++ src/app/page.js
@@ -9,7 +9,7 @@ import Footer from './components/Footer'
 export default function Home() {
   return <main className="flex min-h-screen flex-col bg-[#121212]">
     <Navbar />
-    <div className="container mx-auto px-12 py-4">
+    <div className="container mx-auto px-12 pt-16">
       <HeroSection />
       <AboutSection />
       <ProjectsSection />
